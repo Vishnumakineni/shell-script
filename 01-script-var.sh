ebc8d52462ab8b59e707ddfdc11b2783b73b1140
Vishnumakineni <vishnuvardhan.makineni@gmail.com>
2024-04-21 20:50:04 +0530

    new script

---
+++ 01-script-var.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+$READER=VISHNU
+$WRITER=VARDHAN
+
+echo "i am $READER, howare you $Writer"
+echo "i am $WRITER and iam fine"
+echo "$READER and $WRITER are both friends"
+echo "sample $WRITER"
+echo "thanku $READER"
---
+++ 02-script-var.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+READER=Vishnu
+WRITER=Vardhan
+
+echo "i am $READER,how is $WRITER"
+echo "bothe are same full name is $READER $WRITER"
